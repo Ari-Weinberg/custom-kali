112ee62f817bf05df798cb58c7f95190d7c3b5e0
ari <ari@weinsecure.com>
2024-11-12 20:22:47 -0500

    fix ngrok again

--- root.sh
+++ root.sh
@@ -17,9 +17,9 @@ curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/
 
 # Install ngrok
 curl -sSL https://ngrok-agent.s3.amazonaws.com/ngrok.asc \
-  | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null \
+  | tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null \
   && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" \
-  | sudo tee /etc/apt/sources.list.d/ngrok.list
+  | tee /etc/apt/sources.list.d/ngrok.list
 
 # Packages
 apt update
