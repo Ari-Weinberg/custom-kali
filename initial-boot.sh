d8e96dee17c8c672e1163dd2b3febd8e5adc5a00
ari <ari@weinsecure.com>
2024-11-19 19:59:49 -0500

    fix

--- initial-boot.sh
+++ initial-boot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-xfce4-panel-profiles load $HOME/configs/xfce-panel.conf load
+xfce4-panel-profiles load $HOME/configs/panel.tar.bz2 load
 xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitorVirtual1/workspace0/last-image -s $HOME/Pictures/background.jpg
 
 rm -- "$0"
